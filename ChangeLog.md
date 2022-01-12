a4e948d158f3330ef27fc83548115fc6fcfc0657
Rashad Gover <rashad.sasaki@gmail.com>
2022-01-12 06:21:22 +0000

    Fix attribute functions; update dependency

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,21 @@
 # Changelog for lucid-alpine
 
-## Unreleased changes
+## 0.1.0.1
+
+Upgrade to `lucid-2.10.0`
+
+## 0.1.0.2
+
+Downgrade to `lucid-2.9.12.1` to depend on `servant-lucid-0.9.0.2`
+
+## 0.1.0.3
+
+Depend on fork of `lucid` that retains attribute order
+
+## 0.1.0.4
+
+Remove "data-" prefix from Alpine.js attributes
+
+## 0.1.0.5
+
+Depend on `lucid-2.11.0`
--- src/Lucid/Alpine.hs
+++ src/Lucid/Alpine.hs
@@ -8,10 +8,8 @@ import Lucid.Base (Attribute, makeAttribute)
 
 -- | x-data
 -- Declare a new Alpine component and its data for a block of HTML
-xData_ :: Maybe Text -> Attribute
-xData_ = \case
-  Nothing     -> makeAttribute "data-x-data" mempty
-  Just object -> makeAttribute "data-x-data" object
+xData_ :: Text -> Attribute
+xData_ = makeAttribute "x-data"
 
 {-
 <div x-data="{ open: false }">
@@ -25,7 +23,7 @@ xBind_
   :: Text -- ^ Attribute name
   -> Text
   -> Attribute
-xBind_ attr = makeAttribute ("data-x-bind:" <> attr)
+xBind_ attr = makeAttribute ("x-bind:" <> attr)
 
 {-
 <div x-bind:class="! open ? 'hidden' : ''">
@@ -39,7 +37,7 @@ xOn_
   :: Text -- ^ Event name
   -> Text
   -> Attribute
-xOn_ event = makeAttribute ("data-x-on:" <> event)
+xOn_ event = makeAttribute ("x-on:" <> event)
 
 {-
 <button x-on:click="open = ! open">
@@ -50,7 +48,7 @@ xOn_ event = makeAttribute ("data-x-on:" <> event)
 -- | x-text
 -- Set the text content of an element
 xText_ :: Text -> Attribute
-xText_ = makeAttribute "data-x-text"
+xText_ = makeAttribute "x-text"
 
 {-
 <div>
@@ -63,7 +61,7 @@ xText_ = makeAttribute "data-x-text"
 -- | x-html
 -- Set the inner HTML of an element
 xHtml_ :: Text -> Attribute
-xHtml_ = makeAttribute "data-x-html"
+xHtml_ = makeAttribute "x-html"
 
 {-
 <div x-html="(await axios.get('/some/html/partial')).data">
@@ -78,8 +76,8 @@ xModel_
   -> Text
   -> Attribute
 xModel_ mods = case mods of
-  [] -> makeAttribute "data-x-model"
-  _  -> makeAttribute ("data-x-model." <> intercalate "." mods)
+  [] -> makeAttribute "x-model"
+  _  -> makeAttribute ("x-model." <> intercalate "." mods)
 
 {-
 <div x-data="{ search: '' }">
@@ -92,7 +90,7 @@ xModel_ mods = case mods of
 -- | x-show
 -- Toggle the visibility of an element
 xShow_ :: Text -> Attribute
-xShow_ = makeAttribute "data-x-show"
+xShow_ = makeAttribute "x-show"
 
 {-
 <div x-show="open">
@@ -107,10 +105,10 @@ xTransition_
   -> [Text]     -- ^ List of x-transition modifiers
   -> Text
   -> Attribute
-xTransition_ Nothing [] _ = makeAttribute "data-x-transition" mempty -- No directive or modifiers
-xTransition_ (Just dir) [] attrVal = makeAttribute ("data-x-transition:" <> dir) attrVal -- Directive with custom transition classes
-xTransition_ Nothing mods _ = makeAttribute ("data-x-transition." <> intercalate "." mods) mempty -- No directive, but with modifiers
-xTransition_ (Just dir) mods _ = makeAttribute ("data-x-transition:" <> dir <> "." <> intercalate "." mods) mempty -- Directive with modifiers
+xTransition_ Nothing [] _ = makeAttribute "x-transition" mempty -- No directive or modifiers
+xTransition_ (Just dir) [] attrVal = makeAttribute ("x-transition:" <> dir) attrVal -- Directive with custom transition classes
+xTransition_ Nothing mods _ = makeAttribute ("x-transition." <> intercalate "." mods) mempty -- No directive, but with modifiers
+xTransition_ (Just dir) mods _ = makeAttribute ("x-transition:" <> dir <> "." <> intercalate "." mods) mempty -- Directive with modifiers
 
 {-
 <div x-show="open" x-transition>
@@ -121,7 +119,7 @@ xTransition_ (Just dir) mods _ = makeAttribute ("data-x-transition:" <> dir <> "
 -- | x-for
 -- Repeat a block of HTML based on a data set
 xFor_ :: Text -> Attribute
-xFor_ = makeAttribute "data-x-for"
+xFor_ = makeAttribute "x-for"
 
 xForKey_ :: Text -> Attribute
 xForKey_ = makeAttribute ":key"
@@ -135,7 +133,7 @@ xForKey_ = makeAttribute ":key"
 -- | x-if
 -- Conditionally add/remove a block of HTML from the page entirely.
 xIf_ :: Text -> Attribute
-xIf_ = makeAttribute "data-x-if"
+xIf_ = makeAttribute "x-if"
 
 {-
 <template x-if="open">
@@ -146,7 +144,7 @@ xIf_ = makeAttribute "data-x-if"
 -- | x-init
 -- Run code when an element is initialized by Alpine
 xInit_ :: Text -> Attribute
-xInit_ = makeAttribute "data-x-init"
+xInit_ = makeAttribute "x-init"
 
 {-
 <div x-init="date = new Date()"></div>
@@ -155,7 +153,7 @@ xInit_ = makeAttribute "data-x-init"
 -- | x-effect
 -- Execute a script each time one of its dependancies change
 xEffect_ :: Text -> Attribute
-xEffect_ = makeAttribute "data-x-effect"
+xEffect_ = makeAttribute "x-effect"
 
 {-
 <div x-effect="console.log('Count is '+count)"></div>
@@ -164,7 +162,7 @@ xEffect_ = makeAttribute "data-x-effect"
 -- | x-ref
 -- Reference elements directly by their specified keys using the $refs magic property
 xRef_ :: Text -> Attribute
-xRef_ = makeAttribute "data-x-ref"
+xRef_ = makeAttribute "x-ref"
 
 {-
 <input type="text" x-ref="content">
@@ -177,7 +175,7 @@ xRef_ = makeAttribute "data-x-ref"
 -- | x-cloak
 -- Hide a block of HTML until after Alpine is finished initializing its contents
 xCloak_ :: Attribute
-xCloak_ = makeAttribute "data-x-cloak" mempty
+xCloak_ = makeAttribute "x-cloak" mempty
 
 {-
 <div x-cloak>
@@ -188,7 +186,7 @@ xCloak_ = makeAttribute "data-x-cloak" mempty
 -- | x-ignore
 -- Prevent a block of HTML from being initialized by Alpine
 xIgnore_ :: Attribute
-xIgnore_ = makeAttribute "data-x-ignore" mempty
+xIgnore_ = makeAttribute "x-ignore" mempty
 
 {-
 <div x-ignore>
